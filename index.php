bcdd63e4dbe3869035ab0c833e668a74d1fd6b79
a-aa-ba <a.ivr.maracot@gmail.com>
2026-06-19 06:41:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,47 @@
 <?php
-// הגדרת קידוד
+// הגדרת ראש כותרת תקין
 header('Content-Type: text/plain; charset=utf-8');
 
-// קבלת נתונים מהשרת
-$phone = isset($_GET['Phone']) ? $_GET['Phone'] : '';
-$read = isset($_GET['read']) ? $_GET['read'] : '';
-$step = isset($_GET['step']) ? $_GET['step'] : 'menu'; // ניהול שלבים
+// קבלת פרמטרים
+$step = isset($_REQUEST['step']) ? $_REQUEST['step'] : 'menu';
+$read = isset($_REQUEST['value']) ? $_REQUEST['value'] : ''; // שינוי ל-value כפי שרוב המערכות שולחות
 
-// לוגיקה של ניהול השיחה
 switch ($step) {
     
-    // תפריט ראשי
     case 'menu':
-        echo "id_list_message=t-שלום, הגעת לבנק הוירטואלי. להעברת כסף הקש 1\n";
-        echo "read=fld=action|num=1|min=1|max=1\n";
-        echo "go_to=ivr.php?step=check_action\n";
+        // שימוש ב-id_list_message להשמעה
+        echo "id_list_message=t-שלום, הגעת לבנק הוירטואלי. להעברת כסף הקש 1" . PHP_EOL;
+        // read תקין
+        echo "read=fld=action|num=1|min=1|max=1" . PHP_EOL;
+        // הכתובת אליה המערכת תשלח את הנתונים לאחר ההקשה
+        echo "go_to=ivr.php?step=check_action" . PHP_EOL;
         break;
 
-    // בדיקת בחירת המשתמש
     case 'check_action':
         if ($read == '1') {
-            echo "id_list_message=t-אנא הקש את מספר הטלפון של המשתמש אליו תרצה להעביר את הכסף\n";
-            echo "read=fld=target_phone|min=8|max=10\n"; // 8 עד 10 ספרות (כולל קידומת)
-            echo "go_to=ivr.php?step=ask_amount\n";
+            echo "id_list_message=t-אנא הקש את מספר הטלפון של המשתמש אליו תרצה להעביר את הכסף" . PHP_EOL;
+            echo "read=fld=target_phone|min=8|max=10" . PHP_EOL;
+            echo "go_to=ivr.php?step=ask_amount" . PHP_EOL;
         } else {
-            echo "id_list_message=t-לא נבחרה אפשרות תקינה\n";
-            echo "go_to=ivr.php?step=menu\n";
+            echo "id_list_message=t-לא נבחרה אפשרות תקינה" . PHP_EOL;
+            echo "go_to=ivr.php?step=menu" . PHP_EOL;
         }
         break;
 
-    // בקשת סכום
     case 'ask_amount':
-        $target_phone = $read; // המספר שהוקש בשלב הקודם נשמר ב-$read
-        echo "id_list_message=t-אנא הקש את הסכום להעברה\n";
-        echo "read=fld=amount|min=1|max=10\n";
-        // מעבירים את מספר היעד הלאה ב-URL
-        echo "go_to=ivr.php?step=finish&target_phone=$target_phone\n";
+        // כאן ה-read הוא מספר הטלפון שהוקש
+        $target_phone = $read;
+        echo "id_list_message=t-אנא הקש את הסכום להעברה" . PHP_EOL;
+        echo "read=fld=amount|min=1|max=10" . PHP_EOL;
+        echo "go_to=ivr.php?step=finish&target_phone=$target_phone" . PHP_EOL;
         break;
 
-    // סיום
     case 'finish':
         $amount = $read;
         $target_phone = isset($_GET['target_phone']) ? $_GET['target_phone'] : '';
         
-        // כאן תוסיף את הלוגיקה של עדכון מסד הנתונים (SQL)
-        // לדוגמה: update_balance($phone, $target_phone, $amount);
-        
-        echo "id_list_message=t-הסכום $amount שקלים הועבר בהצלחה למספר $target_phone\n";
-        echo "hangup=yes\n";
+        echo "id_list_message=t-הסכום $amount שקלים הועבר בהצלחה למספר $target_phone" . PHP_EOL;
+        echo "hangup=yes" . PHP_EOL;
         break;
 }
 ?>
